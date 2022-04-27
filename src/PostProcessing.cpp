553f0dd034374c25e7624a3f49d5ba62f25830f2
John Manard <johnmanardiii@gmail.com>
2022-04-27 14:00:03 -0700

    Post processing is working les go

--- src/PostProcessing.cpp
+++ src/PostProcessing.cpp
@@ -71,7 +71,7 @@ PostProcessing::PostProcessing(WindowManager* wm)
 	glGenTextures(1, &base_color);
 	glBindTexture(GL_TEXTURE_2D, base_color);
 	// TODO: make this larger than 8 bit texture for hdr.
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, NULL);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, NULL);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, base_color, 0);
