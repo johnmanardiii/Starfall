d230ffc06f7ae7f65604b0973c7f7bd90372d083
Bob-Loth <bob94loth@gmail.com>
2022-04-28 12:20:56 -0700

    removed unused ground data from Application.h

--- src/Application.h
+++ src/Application.h
@@ -21,35 +21,13 @@
 
 class Application : public EventCallbacks
 {
-
 public:
-	// the component manager.
+	// the various managers that make up the application.
 	ComponentManager componentManager;
 	ShaderManager& shaderManager = ShaderManager::GetInstance();
 	WindowManager* windowManager = nullptr;
 	AudioEngine audioEngine;
-	typedef struct Ground
-	{
-		GLuint VertexArrayID;
-		GLuint buffObj;
-		GLuint norBuffObj;
-		GLuint texBuffObj;
-		GLuint indexBuffObj;
-		int giboLen;
-	} Ground;
-	Ground ground;
-
-	// Our shader program
-	std::shared_ptr<Program> prog;
-
-	// Shape to be used (from  file) - modify to support multiple
-	shared_ptr<Shape> mesh;
-
-	// Contains vertex information for OpenGL
-	GLuint VertexArrayID;
-
-	// Data necessary to give our triangle to OpenGL
-	GLuint vertexBufferID;
+	
 	//callbacks
 	void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods);
 	void mouseCallback(GLFWwindow* window, int button, int action, int mods);
