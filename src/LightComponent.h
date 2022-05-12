4ac00537236de662ad48c82a5178cf201fae6342
Fiona Soetrisno <fiona.soetrisno@gmail.com>
2022-05-11 21:14:02 -0700

    light component

--- src/LightComponent.h
+++ src/LightComponent.h
@@ -9,7 +9,8 @@ private:
 	void Update(float frameTime, ComponentManager* compMan);
 	void Init(ComponentManager* compMan);
 public:
-	vec3 sunDir;
+	vec3 sunDir = vec3(0, 0, 1);
+	float sunRotation = 0; // Sun rotation around x axis
 	static LightComponent& GetInstance(vec3 sunDir) {
 		static LightComponent instance(sunDir);
 		return instance;
